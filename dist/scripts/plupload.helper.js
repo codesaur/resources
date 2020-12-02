fd59a1334bf6afe755ddb9eb8ed4bdbef059d986
Наранхүү <codesaur@gmail.com>
2020-12-02 10:52:03 +0800

    Create plupload.helper.js

--- dist/scripts/plupload.helper.js
+++ dist/scripts/plupload.helper.js
@@ -3,13 +3,15 @@
 (function($) {
     $.fn.Plupload = function(options, callback) {
         var settings = $.extend(true, {
-            container: $(this),
             url: 'javascript:;',
-            runtimes: 'html5,flash,silverlight,html4',
-            flash_swf_url: 'https://cdn.jsdelivr.net/gh/moxiecode/plupload/js/Moxie.swf',
-            silverlight_xap_url: 'https://cdn.jsdelivr.net/gh/moxiecode/plupload/js/Moxie.xap',
+            DOM: {
+                file_list: null,
+                pick_files: null,
+                upload_files: null
+            },
             filters: {
                 max_file_size: '2mb',
+                prevent_duplicates: true,
                 mime_types: [{title: 'Image files', extensions: 'jpg,gif,png'}]
             },
             texts: {
@@ -18,35 +20,34 @@
                 failed:  'Failed',
                 done:    'Done'
             },
-            DOM: {
-                pickFile: null,
-                fileList: null,
-                uploadFiles: null
-            }
+            runtimes: 'html5,flash,silverlight,html4',
+            flash_swf_url: 'https://cdn.jsdelivr.net/gh/moxiecode/plupload/js/Moxie.swf',
+            silverlight_xap_url: 'https://cdn.jsdelivr.net/gh/moxiecode/plupload/js/Moxie.xap'
         }, options);
         
-        settings.browse_button = document.getElementById(settings.DOM.pickFile);
-        
+        settings.container = $(this)[0];
+        settings.browse_button = document.getElementById(settings.DOM.pick_files);
+
         settings.init = {
             PostInit: function() {
-                $('#' + settings.DOM.fileList).html('');
-                $('#' + settings.DOM.uploadFiles).click(function() {
+                $('#' + settings.DOM.file_list).html('');
+                $('#' + settings.DOM.upload_files).click(function() {
                     uploader.start();
                     return false;
                 });
-                $('#' + settings.DOM.fileList).on('click', '.added-files .remove', function() {
-                    uploader.removeFile($(this).parent('.added-files').attr("id"));
+                $('#' + settings.DOM.file_list).on('click', '.added-files .remove', function() {
+                    uploader.removeFile($(this).parent('.added-files').attr('id'));
 
                     $(this).parent('.added-files').remove();
                 });
             },
             FilesAdded: function(up, files) {
                 plupload.each(files, function(file) {
-                   currentFileId = file.id;
-                   $('#' + settings.DOM.fileList).append('<div class="alert alert-info added-files" id="' + file.id + '">' + file.name + '(' + plupload.formatSize(file.size) + ') &nbsp;&nbsp; <span class="status badge badge-info"></span> <a href="javascript:;" style="margin-top:-5px" class="remove pull-right btn btn-sm btn-danger text-lowercase"><i class="fa fa-times"></i> ' + settings.texts.remove + '</a></div>');
+                    $('#' + settings.DOM.file_list).append('<div class="alert alert-info added-files" id="' + file.id + '">' + file.name + '(' + plupload.formatSize(file.size) + ') &nbsp;&nbsp; <span class="status badge badge-info"></span> <a href="javascript:;" style="margin-top:-5px" class="remove pull-right btn btn-sm btn-danger text-lowercase"><i class="fa fa-times"></i> ' + settings.texts.remove + '</a></div>');
                 });
             },
             UploadProgress: function(up, file) {
+                currentFileId = file.id;
                 $('#' + file.id + ' > .status').html(file.percent + '%');
             },
             FileUploaded: function(up, file, response) {
@@ -66,14 +67,15 @@
                 if (currentFileId) {
                     $('#' + currentFileId + ' > .status').removeClass('badge-light').addClass('badge-danger').html('<i class="fa fa-warning"></i> ' + settings.texts.failed);
                 }
-
+                
                 Dashboard.notify('error', settings.texts.warning, err.message);
             }
         };
         
-        var uploader = new plupload.Uploader(settings);
         var currentFileId = null;
 
+        var uploader = new plupload.Uploader(settings);
+        uploader.init();
         return uploader;
     };
 } (jQuery));

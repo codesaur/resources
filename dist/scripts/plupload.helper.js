6dd9e959528e4dd4d4a3133afa279bde8eed7186
Наранхүү <codesaur@gmail.com>
2020-12-04 12:53:39 +0800

    Update plupload.helper.js

--- dist/scripts/plupload.helper.js
+++ dist/scripts/plupload.helper.js
@@ -20,6 +20,9 @@
                 failure: 'Failure',
                 success: 'Success'
             },
+            multipart_params : {
+                real_name : "value"
+            },
             runtimes: 'html5,flash,silverlight,html4',
             flash_swf_url: 'https://cdn.jsdelivr.net/gh/moxiecode/plupload/js/Moxie.swf',
             silverlight_xap_url: 'https://cdn.jsdelivr.net/gh/moxiecode/plupload/js/Moxie.xap'
@@ -31,21 +34,44 @@
         settings.init = {
             PostInit: function() {
                 $('#' + settings.DOM.file_list).html('');
+                
+                $('#' + settings.DOM.upload_files).removeClass('pluploader-ready btn-primary').addClass('btn-secondary');
+                
                 $('#' + settings.DOM.upload_files).click(function() {
-                    uploader.start();
+                    if ($('#' + settings.DOM.upload_files).hasClass('pluploader-ready')) {
+                        uploader.start();
+                    }
+                    
                     return false;
                 });
                 $('#' + settings.DOM.file_list).on('click', '.added-files .remove', function() {
                     uploader.removeFile($(this).parent('.added-files').attr('id'));
 
                     $(this).parent('.added-files').remove();
+                    
+                    if ($('#' + settings.DOM.file_list + ' .added-files').length > 0) {
+                        $('#' + settings.DOM.upload_files).removeClass('btn-secondary').addClass('pluploader-ready btn-primary');
+                    } else {
+                        $('#' + settings.DOM.upload_files).removeClass('pluploader-ready btn-primary').addClass('btn-secondary');
+                    }
                 });
             },
             FilesAdded: function(up, files) {
                 plupload.each(files, function(file) {
                     $('#' + settings.DOM.file_list).append('<div class="alert alert-info added-files" id="' + file.id + '">' + file.name + '(' + plupload.formatSize(file.size) + ') &nbsp;&nbsp; <span class="status badge badge-info"></span> <a href="javascript:;" style="margin-top:-5px" class="remove float-right btn btn-sm btn-danger text-lowercase"><i class="la la-trash"></i> ' + settings.texts.remove + '</a></div>');
                 });
+                
+                if ($('#' + settings.DOM.file_list + ' .added-files').length > 0) {
+                    $('#' + settings.DOM.upload_files).removeClass('btn-secondary').addClass('pluploader-ready btn-primary');
+                } else {
+                    $('#' + settings.DOM.upload_files).removeClass('pluploader-ready btn-primary').addClass('btn-secondary');
+                }
             },
+            /*BeforeUpload: function(up, file) {
+                settings.multipart_params = {
+                    real_name: file.name
+                };
+            },*/
             UploadProgress: function(up, file) {
                 currentFileId = file.id;
                 $('#' + file.id + ' > .status').html(file.percent + '%');
@@ -57,8 +83,9 @@
                     var res = $.parseJSON(response.response);
                     if (!res.data) throw 'Invalid response!';
                     
+                    $('#' + file.id + ' > .remove').remove();
+                    $('#' + file.id).removeClass('added-files');
                     $('#' + file.id + ' > .status').removeClass('badge-light').addClass('badge-success').html('<i class="la la-check"></i> ' + settings.texts.success);
-                    $('#' + file.id + ' > .remove').addClass('hidden d-none');
 
                     if (callback && typeof callback === "function") {
                         callback(file, res);
@@ -73,6 +100,12 @@
                     else if (res && res.error && res.error.message) err = res.error.message;
                     
                     Dashboard.notify('error', settings.texts.failure, err);
+                }
+
+                if ($('#' + settings.DOM.file_list + ' .added-files').length > 0) {
+                    $('#' + settings.DOM.upload_files).removeClass('btn-secondary').addClass('pluploader-ready btn-primary');
+                } else {
+                    $('#' + settings.DOM.upload_files).removeClass('pluploader-ready btn-primary').addClass('btn-secondary');
                 }
             },
             Error: function(up, err) {
